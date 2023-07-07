22a6208b13a364df9a51e9c055008f417486079f
Mina Iosif Alexandru <minaiosifalexandru@yahoo.com>
2023-07-07 18:28:48 +0300

    Add searchbar, login, cart buttons, modify error message, add working navbar links

---
+++ src/components/About/about.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./about.module.css"
+
+function About( e ) {
+    return (
+        <>
+        <p className={styles.test}>About section</p>
+        </>
+    )
+}
+
+export default About
