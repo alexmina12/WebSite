82cf54dec5418929e2821b03ff6d6c5c35590ebb
Mina Iosif Alexandru <minaiosifalexandru@yahoo.com>
2023-07-21 18:05:33 +0300

    Add new register and login

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -1,12 +1,60 @@
-import React from "react";
-import styles from "./login.module.css"
+// Login.jsx
+import React, { useState } from "react";
+import styles from "./login.module.css";
+import axios from 'axios';
 
-function Login( e ) {
-    return (
-        <>
-        <p className={styles.test}>Login section</p>
-        </>
-    )
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginStatus, setLoginStatus] = useState(null);
+
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    if (id === "email") {
+      setEmail(value);
+    } else if (id === "password") {
+      setPassword(value);
+    }
+  };
+
+  const handleLogin = () => {
+    const userData = {
+      email,
+      password
+    };
+
+    axios.post('http://localhost:3000/login', userData)
+      .then(response => {
+        console.log(response.data);
+        // Setează starea de login cu răspunsul de la server
+        setLoginStatus(response.data.message);
+      })
+      .catch(error => {
+        console.error(error);
+        // Tratarea erorilor
+        setLoginStatus('A apărut o eroare la autentificare.');
+      });
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Login</h1>
+      {loginStatus && (
+        <p className={styles.loginStatus}>{loginStatus}</p>
+      )}
+      <div className={styles.formGroup}>
+        <label className={styles.label} htmlFor="email">Email</label>
+        <input className={styles.input} type="email" id="email" placeholder="Email" onChange={handleInputChange} />
+      </div>
+      <div className={styles.formGroup}>
+        <label className={styles.label} htmlFor="password">Password</label>
+        <input className={styles.input} type="password" id="password" placeholder="Password" onChange={handleInputChange} />
+      </div>
+      <div className={styles.login}>
+        <button onClick={handleLogin} type="submit" className={styles.btn}>Login</button>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
