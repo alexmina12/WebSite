d04ce271d7a84e23cf9612d93f16b1b9fbd5d3c9
Mina Iosif Alexandru <minaiosifalexandru@yahoo.com>
2023-07-23 13:04:41 +0300

    Style components and resolve footer year error

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -80,7 +80,7 @@ export function Footer() {
             <div className={styles['footer-below']}>
                 <div className={styles['footer-copyright']}>
                     <p>
-                        @(new Date().getFullYear()) ShopWise, All rights reserved &copy; 
+                        {(new Date().getFullYear())} ShopWise, All rights reserved &copy; 
                     </p>
                 </div>
                 <div className={styles['footer-below-links']}>
